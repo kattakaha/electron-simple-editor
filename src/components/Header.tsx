62126e5599b3f56adf6c06077f0bd43b12be17c6
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-04-06 10:42:24 +0900

    :sparkles: add font size change handler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,20 @@ import DownloadButton from "./atoms/DownloadButton";
 import MinusButton from "./atoms/MinusButton";
 import PlusButton from "./atoms/PlusButton";
 
-export default function Header() {
+interface HeaderProps {
+  textAreaRef: React.RefObject<HTMLTextAreaElement>;
+}
+
+export default function Header({ textAreaRef }: HeaderProps) {
   return (
     <header className="toolbar toolbar-header">
       <h1 className="title">Header</h1>
       <div className="toolbar-actions">
         <div className="btn-group">
-          <PlusButton />
-          <MinusButton />
+          <PlusButton textAreaRef={textAreaRef} />
+          <MinusButton textAreaRef={textAreaRef} />
         </div>
-        <DownloadButton />
+        <DownloadButton textAreaRef={textAreaRef} />
       </div>
     </header>
   );
