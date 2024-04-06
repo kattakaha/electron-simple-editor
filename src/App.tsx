ec50455031400fce9b1ee2da6057d2c09ffd5c25
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-04-06 09:51:03 +0900

    install photon

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/electron-vite.animate.svg";
-import "./App.css";
+import Header from "./components/Header";
+import "./plugin/photon/css/photon.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a
-          href="https://electron-vite.github.io"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="window">
+      <Header />
+      <div className="window-content">
+        <div className="padded-more">
+          <h1>Hello, World!</h1>
+          <p>
+            Thanks for downloading Photon. This is an example HTML page that is
+            linked up to compiled Photon CSS, has the proper meta tags and the
+            HTML structure.
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="toolbar toolbar-header">
+      <h1 className="title">Header</h1>
+    </header>
+  );
+}
